25b32a2bc8b0d9733ec115eeb3e1a6fb44bd6706
SnakyDH <skdanielg5@gmail.com>
2025-04-26 13:42:25 -0500

    feat: update some components utils for the development

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -1,9 +1,28 @@
 import { useState } from "react";
 import { Button } from "./components/ui/button";
 import "./App.css";
+import { format } from "date-fns";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./components/ui/sheet";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
+import React from "react";
+import { cn } from "./lib/utils";
+import { Calendar } from "./components/ui/calendar";
+import { CalendarIcon } from "lucide-react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [date, setDate] = React.useState<Date>();
 
   return (
     <>
@@ -15,6 +34,44 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+
+        <Sheet>
+          <SheetTrigger>
+            <Button>Open Cart Sheet</Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant={"outline"}
+              className={cn(
+                "w-[240px] justify-start text-left font-normal",
+                !date && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon />
+              {date ? format(date, "PPP") : <span>Pick a date</span>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
       </div>
     </>
   );
